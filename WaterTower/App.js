bbecb2f6693d25ccc21c5ed66f3d69dae23d9cd1
Sudeep Potluri <sspfzr@umsystem.edu>
2022-04-09 11:28:37 -0500

    Setting Page start

--- WaterTower/App.js
+++ WaterTower/App.js
@@ -1,6 +1,6 @@
 import { WaterView } from './WaterView.js';
-
+import { DataTextInput } from './Settings.js';
 
 export default function App() {
-	return WaterView();
+	return DataTextInput();
 }
---
+++ WaterTower/Settings.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Text, View, TextInput } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+export const DataTextInput = () => {
+    const [number,OnChangeNumber] = React.useState(null);
+
+    return (
+        <SafeAreaView>
+            <Text>
+                Settings Page 
+            </Text>
+            <TextInput
+                OnChangeNumber = {OnChangeNumber}
+                value ={number}
+                placeholder = "Please enter amount of of Cum you drink"
+            />
+        </SafeAreaView>
+    );
+};
+
+
+
+    
+
+
