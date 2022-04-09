f54eb8bc301b3673406fd32b05f52f05be56f09b
Noorquacker <noorquacker@nqind.com>
2022-04-08 19:30:07 -0500

    it's my project too and i get to put whatever i want

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Momento de bruh</Text>
+      <Image
+        style={styles.bruh}
+        source={{
+          uri: 'https://c.wallhere.com/photos/de/ab/anime_anime_girls-1779929.jpg!d',
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +22,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bruh: {
+    width: 500,
+    height: 500,
   },
 });
