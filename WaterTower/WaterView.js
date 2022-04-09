b5d4a5baf050e6111b98e081d79f031eb7e558f3
Noorquacker <noorquacker@nqind.com>
2022-04-09 17:23:17 -0500

    Ah frick a merge conflict

--- WaterTower/WaterView.js
+++ WaterTower/WaterView.js
@@ -1,15 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
 import React from 'react';
 
-export const WaterView = ({navigation}) => {
 
+function addWater() {
+
+}
+
+export const WaterView = ({navigation}) => {
 	return (
 	<View style={styles.container}>
 		<Text>Todo: make this work lmao [done]</Text>
-		<Button title="CLICK ON MEEE" onPress={() => {
+		<Pressable onPress={() => {
 			navigation.navigate('Settings');
-		}}/>
+		}}>
+			<Image style={styles.settingsIcon} source={require('./assets/settings.png')} />
+		</Pressable>
+		<Pressable onPress={addWater}>
+			<View style={styles.wContainer}>
+				<Image style={styles.wImage} source={require('./assets/WaterTower.jpeg')}/>
+			</View>
+		</Pressable>
 		<StatusBar style="auto" />
 	</View>
 	);
@@ -19,7 +30,17 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
+	},
+	wContainer: {
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	wImage: {
+		height: 512,
+		width: 270,
+	},
+	settingsIcon: {
+		height: 30,
+		width: 30,
 	},
 });
